aaeedb4a380d891b19a0f4bf8de61961a71f3851
Jack (Pixegami) <pixegami@gmail.com>
2023-10-14 15:36:49 +1100

    updated gitignore

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import setup, find_packages
 
+
+with open("README.md", "r") as f:
+    description = f.read()
+
 setup(
     name="pixegami_hello",
-    version="0.1",
+    version="0.3.0",
     packages=find_packages(),
     install_requires=[
         # Add dependencies here.
@@ -13,4 +17,6 @@ setup(
             "pixegami-hello = pixegami_hello:hello",
         ],
     },
+    long_description=description,
+    long_description_content_type="text/markdown",
 )
